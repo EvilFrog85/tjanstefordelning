e90ea44c562acdd7826ef18100ddcf41257cc67c
Björn Green <berra_@hotmail.com>
2017-05-09 18:24:51 +0200

    added personnelcontroller

---
+++ WebApp/Controllers/PersonnelController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Models.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApp.Controllers
+{
+    public class PersonnelController : Controller
+    {
+        TFContext _context;
+        UserManager<IdentityUser> _userManager;
+        public PersonnelController(UserManager<IdentityUser> userManager, TFContext context)
+        {
+            _userManager = userManager;
+            _context = context; 
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
